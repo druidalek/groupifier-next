623a72c4bf6535bef1bed5eedd2d10fd0c8c376c
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-01-03 18:39:30 +0100

    Updates

---
+++ precache-manifest.277ea2cf3bb92629a877a580da1dffbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e8d9a9271ce6c221197",
+    "url": "/static/js/main.7e8d9a92.chunk.js"
+  },
+  {
+    "revision": "cc219b9bde35c4a43b42",
+    "url": "/static/js/1.cc219b9b.chunk.js"
+  },
+  {
+    "revision": "7e8d9a9271ce6c221197",
+    "url": "/static/css/main.4ba67ff8.chunk.css"
+  },
+  {
+    "revision": "b2c197f192927df44bbe268b57594b6a",
+    "url": "/index.html"
+  }
+];
