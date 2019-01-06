e4afb0537378001506b0edb7f8de30e26b193ba0
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-01-06 11:52:28 +0100

    Updates

--- precache-manifest.6b98a98bea0fa6afecc5b730fa8df0df.js
+++ precache-manifest.6b98a98bea0fa6afecc5b730fa8df0df.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/groupifier-next/static/js/runtime~main.fd0bff10.js"
   },
   {
-    "revision": "adb3e990d53f6413ec02",
-    "url": "/groupifier-next/static/js/main.adb3e990.chunk.js"
+    "revision": "3bf3bce6ace10fe8998d",
+    "url": "/groupifier-next/static/js/main.3bf3bce6.chunk.js"
   },
   {
     "revision": "b5fe16facbc881b7a2d5",
     "url": "/groupifier-next/static/js/1.b5fe16fa.chunk.js"
   },
   {
-    "revision": "adb3e990d53f6413ec02",
+    "revision": "3bf3bce6ace10fe8998d",
     "url": "/groupifier-next/static/css/main.b22f9b8e.chunk.css"
   },
   {
-    "revision": "23b7e6412e405ec6f6767625d26cd189",
+    "revision": "feb09e0aa67f5dffda30595544fa65e0",
     "url": "/groupifier-next/index.html"
   }
 ];
