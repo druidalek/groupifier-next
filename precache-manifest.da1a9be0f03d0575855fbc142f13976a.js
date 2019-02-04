d4653d70ad50681b48f715101073aaab0c0bfd19
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-02-04 10:52:45 +0100

    Updates

---
+++ precache-manifest.da1a9be0f03d0575855fbc142f13976a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd0bff10050bc9967a23",
+    "url": "/groupifier-next/static/js/runtime~main.fd0bff10.js"
+  },
+  {
+    "revision": "b4b430da7c8c718f8042",
+    "url": "/groupifier-next/static/js/main.b4b430da.chunk.js"
+  },
+  {
+    "revision": "6a63b3251c6d9baafbca",
+    "url": "/groupifier-next/static/js/1.6a63b325.chunk.js"
+  },
+  {
+    "revision": "b4b430da7c8c718f8042",
+    "url": "/groupifier-next/static/css/main.07da4b33.chunk.css"
+  },
+  {
+    "revision": "548d852b518a32b3f70fa1b6989ab22f",
+    "url": "/groupifier-next/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/groupifier-next/precache-manifest.ed32c38715a63fcc317f4e74cedf02a7.js"
+  "/groupifier-next/precache-manifest.da1a9be0f03d0575855fbc142f13976a.js"
 );
 
 workbox.clientsClaim();
